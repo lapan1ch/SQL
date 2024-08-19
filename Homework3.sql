504e9f3f5532dcfd399406b933e22f4ab620de9c
lapan1ch <120596545+lapan1ch@users.noreply.github.com>
2024-08-19 21:50:23 +0300

    H3
    My work

---
+++ Homework3.sql
@@ -0,0 +1,108 @@
+create database tasks;
+
+
+drop database tasks;
+
+
+create database tasks;
+
+use tasks;
+
+
+create table employees(
+ 	employee_id int primary key auto_increment,
+ 	first_name varchar(128) not null,
+ 	last_name varchar(128) not null,
+ 	email varchar(128) not null,
+ 	salary int, 
+	department varchar(128) not null
+ );
+ 
+insert into employees(first_name, last_name, email, salary, department) values("Steven","King",	"SKING", 24000, "Sales");
+insert into employees(first_name, last_name, email, salary, department) values("Neena" , "Kochhar" , "NKOCHHAR" , 17000 , "Sales");
+insert into employees(first_name, last_name, email, salary, department) values("Lex" , "De Haan" , "LDEHAAN" , 17000 , "Sales");
+insert into employees(first_name, last_name, email, salary, department) values("Alexander" , "Hunold" , "AHUNOLD" , 9000 , "Finance");
+insert into employees(first_name, last_name, email, salary, department) values("Bruce" , "Ernst" , "BERNST" , 6000 , "Finance");
+insert into employees(first_name, last_name, email, salary, department) values("Valli" , "Pataballa" , "VPATABAL" , 4800 , "Finance");
+insert into employees(first_name, last_name, email, salary, department) values("Diana" , "Lorentz" , "DIANALO" , 8800 , "Finance");
+insert into employees(first_name, last_name, email, salary, department) values("Nancy" , "Greenberg" , "NGREENBE" , 12008 , "Shipping");
+insert into employees(first_name, last_name, email, salary, department) values("Daniel" , "Faviet" , "DFAVIET" , 9000 , "Shipping");
+insert into employees(first_name, last_name, email, salary, department) values("Jose Manuel" , "Urman" , "JMURMAN" , 7800 , "Shipping");
+insert into employees(first_name, last_name, email, salary, department) values("Luis" , "Popp" , "LPOPP" , 6900 , "Shipping");
+insert into employees(first_name, last_name, email, salary, department) values("Den" , "Raphaely" , "DRAPHEAL" , 11000 , "Marketing");
+insert into employees(first_name, last_name, email, salary, department) values("Alexander" , "Khoo" , "AKHOO" , 3100 , "Marketing");
+insert into employees(first_name, last_name, email, salary, department) values("Shelli" , "Baida" , "SBAIDA" , 2900 , "Marketing");
+insert into employees(first_name, last_name, email, salary, department) values("Sigal" , "Tobias" , "STOBIAS" , 2800 , "Marketing");
+insert into employees(first_name, last_name, email, salary, department) values("Matthew" , "Weiss" , "MWEISS" , 8000 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Adam" , "Fripp" , "AFRIPP" , 8200 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Payam" , "Kaufling" , "PKAUFLIN" , 7900 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Shanta" , "Vollman" , "SVOLLMAN" , 6500 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Kevin" , "Mourgos" , "KMOURGOS" , 5800 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Julia" , "Nayer" , "JNAYER" , 3200 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Adam" , "Markle" , "SMARKLE" , 2200 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Laura" , "Bissot" , "LBISSOT" , 3300 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Mozhe" , "Atkinson" , "MATKINSO" , 2800 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Joshua" , "Patel" , "JPATEL" , 2500 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Trenna" , "Rajs" , "TRAJS" , 3500 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("John" , "Russell" , "JRUSSEL" , 14000 , "IT");
+insert into employees(first_name, last_name, email, salary, department) values("Karen" , "Partners" , "KPARTNER" , 13500 , "IT");
+insert into employees(first_name, last_name, email, salary, department) values("Alberto" , "Errazuriz" , "AERRAZUR" , 12000 , "IT");
+insert into employees(first_name, last_name, email, salary, department) values("Gerald" , "Cambrault" , "GCAMBRAU" , 11000 , "IT");
+insert into employees(first_name, last_name, email, salary, department) values("Eleni" , "Zlotkey" , "EZLOTKEY" , 10500 , "IT");
+insert into employees(first_name, last_name, email, salary, department) values("Adam" , "Vargas" , "PVARGAS" , 2500 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Laura" , "Errazuriz" , "AERRAZUR" , 12000 , "IT");
+
+SELECT first_name, last_name, department, salary,
+       case
+           when department = 'sales' then salary * 1.2
+           when department = 'finance' then salary * 1.15
+           when department = 'shipping' then salary * 1.1
+           when department = 'marketing' then salary * 1.25
+           when department = 'Human Resources' then salary * 1.2
+           when department = 'IT' then salary * 1.35
+           else salary
+       end as new_salary
+from employees;
+
+alter table employees
+add column new_salary numeric(7, 2);
+
+update employees
+set new_salary = case
+                    when department = 'sales' then salary * 1.2
+                    when department = 'finance' then salary * 1.15
+                    when department = 'shipping' then salary * 1.1
+                    when department = 'marketing' then salary * 1.25
+                    when department = 'Human Resources' then salary * 1.2
+                    when department = 'IT' then salary * 1.35
+                    else salary
+                 end;
+                 
+select first_name as name, last_name as surname, salary as total_salary
+from employees;
+
+alter table employees
+add column commission integer;
+
+select first_name, last_name, salary,
+       case
+           when salary < 3000 then 10
+           when salary < 6000 then 15
+           when salary < 9000 then 20
+           else 20
+       end as commission
+from employees;
+
+alter table employees
+add column finalsalary numeric(7, 2);
+
+update employees
+set finalsalary = coalesce(salary + salary * commission / 100, salary);
+
+
+delete from employees
+where commission < 15;
+
+
+
+
